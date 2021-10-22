599ce2cc29d211b5ec6c280fcd2a209e38fd07c0
sebamar88 <sebamar88@gmail.com>
2021-10-21 21:44:52 -0300

    Spinner Added

--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,35 +1,45 @@
 import Product from "./Product/Product";
 import { useState, useEffect } from "react";
+import axios from 'axios';
+import Spinner from "./UI/Spinner";
 
 const ItemListContainer = ({ title, setCart, cart }) => {
   const [products, setProducts] = useState([]);
+  const [load, setLoad] = useState(false);
   useEffect(() => {
     const API = async () => {
       const url = "https://api-furnistar.prestoapi.com/api/products";
-      const response = await fetch(url);
-      const apiProducts = await response.json();
-      setProducts(apiProducts);
+      const resultado = await axios.get(url);
+      setLoad(true)
+      setTimeout(() => {
+        setLoad(false)
+        setProducts(resultado.data);
+      }, 1500)
+
     };
     API();
   }, []);
+
+
   return (
     <div className="bg-white">
       <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
         <h2 className="text-2xl font-extrabold tracking-tight text-gray-900">
           {title}
         </h2>
-
-        <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
-          {products.length > 1
-            ? products.map((product) => (
-                <Product
-                  setCart={setCart}
-                  cart={cart}
-                  key={product.id}
-                  item={product}
-                />
-              ))
-            : null}
+        <div className={load ? 'mt-6' : 'mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8'}>
+        
+          {load
+            ? <Spinner /> : 
+            products.map((product) => (
+              <Product
+                setCart={setCart}
+                cart={cart}
+                key={product.id}
+                item={product}
+              />
+            ))
+            }
         </div>
       </div>
     </div>
