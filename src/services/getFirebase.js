b06e16a893f71f6b80d19671d6b6ba9379cb37cb
Sebastián Martínez <sebamar88@gmail.com>
2021-11-18 22:03:37 -0300

    Added Environment variables

--- src/services/getFirebase.js
+++ src/services/getFirebase.js
@@ -1,23 +1,21 @@
-import firebase from 'firebase/compat/app'
-import 'firebase/compat/firestore';
-
-
+import firebase from "firebase/compat/app";
+import "firebase/compat/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBrtY32vbxl9CsnG-NP8kfKPUzRwm51Kho",
-  authDomain: "tailwind-ecommerce-coder.firebaseapp.com",
-  projectId: "tailwind-ecommerce-coder",
-  storageBucket: "tailwind-ecommerce-coder.appspot.com",
-  messagingSenderId: "1022896518832",
-  appId: "1:1022896518832:web:beddd92b96b4fe48ebed75",
-  measurementId: "G-N7SGQNG0D4"
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
+  measurementId: process.env.REACT_APP_measurementId,
 };
-const app= firebase.initializeApp(firebaseConfig)
+const app = firebase.initializeApp(firebaseConfig);
 
 // export function getFirebase(){
 //     return app
 // }
 
-export function getFirestore(){
-    return firebase.firestore(app)
+export function getFirestore() {
+  return firebase.firestore(app);
 }
