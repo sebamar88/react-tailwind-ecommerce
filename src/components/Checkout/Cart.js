6f9dde907cce600f07e32657f7b7956cd9d98150
Sebastian <sebamar88@gmail.com>
2022-06-22 01:10:17 -0300

    Changes in Order Detail

--- src/components/Checkout/Cart.js
+++ src/components/Checkout/Cart.js
@@ -1,36 +1,37 @@
-import { Link } from "react-router-dom";
-import { useCartContext } from "../../context/cartContext";
-import PriceTotal from "./PriceTotal";
-import FormCount from "../FormCount/FormCount";
+import { Link } from 'react-router-dom';
+import { useCartContext } from '../../context/cartContext';
+import PriceTotal from './PriceTotal';
+import FormCount from '../FormCount/FormCount';
 
 const Cart = () => {
   const { cartList, removeProduct, orderReady, orderId } = useCartContext();
   return (
     <div className="container py-10">
-      {orderReady ? (
-        <div className="flex items-center flex-col justify-center text-center h-96">
-          <h2 className="text-3xl text-gray-700 font-extrabold mb-8">
+      {orderReady && (
+        <div className="flex flex-col items-center justify-center text-center h-96">
+          <h2 className="mb-8 text-3xl font-extrabold text-gray-700">
             Thank you for your order! <br />
             Your Order # is: {orderId}
           </h2>
           <Link to={`/order/${orderId}`}>
             <button
               type="button"
-              className="bg-indigo-500 hover:bg-indigo-700 text-white text-center font-bold py-3 px-4 rounded"
+              className="px-4 py-3 font-bold text-center text-white bg-indigo-500 rounded hover:bg-indigo-700"
             >
               Watch your order
             </button>
           </Link>
         </div>
-      ) : cartList.length === 0 ? (
-        <div className="flex items-center flex-col justify-center text-center h-96">
-          <h2 className="text-3xl text-gray-700 font-extrabold mb-8">
+      )}
+      {!orderReady && cartList.length === 0 ? (
+        <div className="flex flex-col items-center justify-center text-center h-96">
+          <h2 className="mb-8 text-3xl font-extrabold text-gray-700">
             Empty Basket
           </h2>
-          <Link to={"/"}>
+          <Link to={'/'}>
             <button
               type="button"
-              className="bg-indigo-500 hover:bg-indigo-700 text-white text-center font-bold py-3 px-4 rounded"
+              className="px-4 py-3 font-bold text-center text-white bg-indigo-500 rounded hover:bg-indigo-700"
             >
               Continue Shopping
             </button>
@@ -38,45 +39,43 @@ const Cart = () => {
         </div>
       ) : (
         <>
-          <div className="mt-6 grid grid-cols-1 gap-y-10 px-4 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
-            {cartList.map((item, index) => {
+          <div className="grid grid-cols-1 px-4 mt-6 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+            {cartList.map((item) => {
               return (
-                <>
-                  <div
-                    key={index.id}
-                    className="group relative flex flex-col justify-between"
-                  >
-                    <div className="w-full min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-80 lg:aspect-none">
-                      <img
-                        src={item.product.image}
-                        alt={item.product.title}
-                        className="w-full h-full object-center object-cover lg:w-full lg:h-full"
-                      />
-                    </div>
-                    <div className="mb-4">
-                      <h3 className="text-sm text-gray-700 font-extrabold">
-                        <span aria-hidden="true" className="inset-0" />
-                        {item.product.title}
-                      </h3>
-                      <p className="mt-1 text-sm text-gray-500 capitalize font-bold">
-                        {item.product.category}
-                      </p>
-                      <p className="text-md mt-3 text-green-600 font-bold">{`Price :$${item.product.price}`}</p>
-                      <p className="text-md text-indigo-700 font-bold">{`Qty :  ${item.quantity} `}</p>
-                      <p className="text-md text-green-600 font-bold">
-                        Sub total : $
-                        {parseInt(item.quantity) * parseInt(item.product.price)}
-                      </p>
-                    </div>
-                    <button
-                      type="button"
-                      className="bg-red-500 hover:bg-red-700 text-white text-center font-bold py-3 px-4 rounded"
-                      onClick={() => removeProduct(item.product.id)}
-                    >
-                      ELIMINAR PRODUCTO{" "}
-                    </button>
+                <div
+                  key={item.product.id}
+                  className="relative flex flex-col justify-between group"
+                >
+                  <div className="w-full overflow-hidden bg-gray-200 rounded-md min-h-80 aspect-w-1 aspect-h-1 group-hover:opacity-75 lg:h-80 lg:aspect-none">
+                    <img
+                      src={item.product.image}
+                      alt={item.product.title}
+                      className="object-cover object-center w-full h-full lg:w-full lg:h-full"
+                    />
                   </div>
-                </>
+                  <div className="mb-4">
+                    <h3 className="text-sm font-extrabold text-gray-700">
+                      <span aria-hidden="true" className="inset-0" />
+                      {item.product.title}
+                    </h3>
+                    <p className="mt-1 text-sm font-bold text-gray-500 capitalize">
+                      {item.product.category}
+                    </p>
+                    <p className="mt-3 font-bold text-green-600 text-md">{`Price :$${item.product.price}`}</p>
+                    <p className="font-bold text-indigo-700 text-md">{`Qty :  ${item.quantity} `}</p>
+                    <p className="font-bold text-green-600 text-md">
+                      Sub total : $
+                      {parseInt(item.quantity) * parseInt(item.product.price)}
+                    </p>
+                  </div>
+                  <button
+                    type="button"
+                    className="px-4 py-3 font-bold text-center text-white bg-red-500 rounded hover:bg-red-700"
+                    onClick={() => removeProduct(item.product.id)}
+                  >
+                    ELIMINAR PRODUCTO{' '}
+                  </button>
+                </div>
               );
             })}
           </div>
--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -1,19 +1,19 @@
-import { useState, useEffect } from "react";
-import Spinner from "../UI/Spinner";
-import { useParams } from "react-router-dom";
-import { getFirestore } from "../../services/getFirebase";
+import { useState, useEffect } from 'react';
+import Spinner from '../UI/Spinner';
+import { useParams } from 'react-router-dom';
+import { getFirestore } from '../../services/getFirebase';
 
 const Order = () => {
   const [products, setProducts] = useState([]);
   const [customer, setCustomer] = useState(null);
   const [load, setLoad] = useState(true);
   const params = useParams();
-  const id = params.orderId !== undefined ? params.orderId : "";
+  const id = params.orderId !== undefined ? params.orderId : '';
 
   useEffect(() => {
     const db = getFirestore();
 
-    const query = db.collection("orders").doc(`${id}`);
+    const query = db.collection('orders').doc(`${id}`);
 
     query
       .get()
@@ -26,9 +26,12 @@ const Order = () => {
       .finally(() => setLoad(false));
   }, [id]);
 
+  const totalLong = products.reduce((acum, valor) => acum + valor.price, 0);
+  const total = parseFloat(totalLong).toFixed(2);
+
   return (
     <div className="flex flex-col py-12">
-      <h1 className="text-indigo-900 font-extrabold text-xl mb-2">
+      <h1 className="mb-2 text-xl font-extrabold text-indigo-900">
         Items in order # {id}
       </h1>
 
@@ -37,50 +40,50 @@ const Order = () => {
       ) : (
         <>
           <h3 className="text-lg font-medium">
-            Customer Name:{" "}
-            <span className="text-semibold text-indigo-500">
+            Customer Name:{' '}
+            <span className="text-indigo-500 text-semibold">
               {customer.firstName} {customer.lastName}
             </span>
           </h3>
           <h3 className="text-lg font-medium">
-            Customer Email:{" "}
-            <span className="text-semibold text-indigo-500">
+            Customer Email:{' '}
+            <span className="text-indigo-500 text-semibold">
               {customer.emailAdress}
             </span>
           </h3>
           <h3 className="text-lg font-medium">
-            Customer Phone:{" "}
-            <span className="text-semibold text-indigo-500">
+            Customer Phone:{' '}
+            <span className="text-indigo-500 text-semibold">
               {customer.phone}
             </span>
           </h3>
           <div className="my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-            <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
-              <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+            <div className="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
+              <div className="overflow-hidden border-b border-gray-200 shadow sm:rounded-lg">
                 <table className="min-w-full divide-y divide-gray-200">
                   <thead className="bg-indigo-50">
                     <tr>
                       <th
                         scope="col"
-                        className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                        className="px-6 py-3 text-xs font-medium tracking-wider text-center text-gray-500 uppercase"
                       >
                         Quantity
                       </th>
                       <th
                         scope="col"
-                        className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                        className="px-6 py-3 text-xs font-medium tracking-wider text-center text-gray-500 uppercase"
                       >
                         Item
                       </th>
                       <th
                         scope="col"
-                        className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                        className="px-6 py-3 text-xs font-medium tracking-wider text-center text-gray-500 uppercase"
                       >
                         Unitary Price
                       </th>
                       <th
                         scope="col"
-                        className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                        className="px-6 py-3 text-xs font-medium tracking-wider text-center text-gray-500 uppercase"
                       >
                         Subtotal
                       </th>
@@ -89,32 +92,44 @@ const Order = () => {
                   <tbody className="bg-white divide-y divide-gray-200">
                     {products.map((product) => (
                       <tr key={product.id}>
-                        <td className="px-6 py-4 whitespace-nowrap">
+                        <td className="px-6 py-4 text-center whitespace-nowrap">
                           <div className="flex items-center">
-                            <div className="flex-shrink-0 h-10 w-10">
+                            <div className="flex-shrink-0 w-10 h-10">
                               <span>{product.quantity} x</span>
                             </div>
                           </div>
                         </td>
-                        <td className="px-6 py-4 whitespace-nowrap">
+                        <td className="px-6 py-4 text-center whitespace-nowrap">
                           <div className="text-sm text-gray-900">
                             {product.title}
                           </div>
                         </td>
-                        <td className="px-6 py-4 whitespace-nowrap">
-                          <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
-                            $ {product.price}
+                        <td className="px-6 py-4 text-center whitespace-nowrap">
+                          <span className="inline-flex px-2 text-xs font-semibold leading-5 text-green-800 bg-green-100 rounded-full">
+                            ${' '}
+                            {parseFloat(
+                              product.price / product.quantity
+                            ).toFixed(2)}
                           </span>
                         </td>
-                        <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                          ${" "}
-                          {parseFloat(product.price * product.quantity).toFixed(
-                            2
-                          )}
+                        <td className="px-6 py-4 text-sm text-center text-gray-500 whitespace-nowrap">
+                          $ {product.price}
                         </td>
                       </tr>
                     ))}
                   </tbody>
+                  <tfoot className="bg-indigo-200">
+                    <tr>
+                      <td className="py-4 text-sm font-extrabold text-center text-gray-500 border-4 border-r-0 border-indigo-500 whitespace-nowrap">
+                        TOTAL
+                      </td>
+                      <td className="py-4 text-sm font-extrabold text-center text-gray-500 border-4 border-l-0 border-r-0 border-indigo-500 whitespace-nowrap"></td>
+                      <td className="py-4 text-sm font-extrabold text-center text-gray-500 border-4 border-l-0 border-r-0 border-indigo-500 whitespace-nowrap"></td>
+                      <td className="py-4 text-sm font-extrabold text-center text-gray-500 border-4 border-l-0 border-indigo-500 whitespace-nowrap">
+                        $ {total}
+                      </td>
+                    </tr>
+                  </tfoot>
                 </table>
               </div>
             </div>
